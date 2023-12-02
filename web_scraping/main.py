6c5cb77616056f6b1526d7f73ed620357de6b455
JC-Guerra <guerrajonathancarl@gmail.com>
2023-12-02 17:05:09 +0800

    added scraping for temperature listed

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 def get_driver():
 #Set options to make browsing easier
@@ -15,9 +16,22 @@ def get_driver():
     driver.get("https://automated.pythonanywhere.com/")
     return driver
 
-def main():
-    driver = get_driver()
-    element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
-    return element.text
+def get_temp(temp):
+    """Extracting temp value only"""
+    try:
+        element = float(temp[-2:])
+    except:
+        print("Increase time.sleep(s)")
+    return element
 
-print(main())
+def obtain_values(header, temp):
+    driver = get_driver()
+    time.sleep(3)
+    element_header = driver.find_element(By.XPATH, header)
+    element_temp = driver.find_element(By.XPATH, temp)
+    return (element_header.text, get_temp(element_temp.text))
+
+scraped_values = obtain_values("/html/body/div[1]/div/h1[1]", "/html/body/div[1]/div/h1[2]")
+
+
+print(f"header: {scraped_values[0]} \nworld temperature: {scraped_values[1]}")
